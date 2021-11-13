bb0d3918c181c0fdb867b6f11bb192dd3b99fa32
Igor Buzykin <lexemz@gmail.com>
2021-11-13 22:14:20 +0300

    Initial Commit

---
+++ Contact List/Contact_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Contact_ListApp.swift
+//  Contact List
+//
+//  Created by Igor on 13.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct Contact_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
